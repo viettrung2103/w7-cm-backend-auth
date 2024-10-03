802db732db3ab68cc9a638b0343264b9056af5fa
Trung_Doan <44894608+viettrung2103@users.noreply.github.com>
2024-10-04 01:08:47 +0300

    add nodemon

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const jobRouter = require("./routes/jobRouter");
 const userRouter = require("./routes/userRouter");
-const { unknownEndpoint,errorHandler } = require("./middleware/customMiddleware");
+const {
+  unknownEndpoint,
+  errorHandler,
+} = require("./middleware/customMiddleware");
 const connectDB = require("./config/db");
 const cors = require("cors");
+const nodemon = require("nodemon");
 
 // Middlewares
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 connectDB();
+
+app.use(morgan("dev"));
 
 // Use the jobRouter for all "/jobs" routes
 app.use("/api/jobs", jobRouter);
 // Use the userRouter for all "/jobs" routes
 app.use("/api/users", userRouter);
-
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
@@ -26,4 +31,4 @@ module.exports = app;
 
 // app.listen(process.env.PORT, () => {
 //   console.log(`Server running on port ${process.env.PORT}`)
-// })  
+// })
